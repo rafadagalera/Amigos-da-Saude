87567ab23743ddb7c0a47830ce42ab048e967cc7
dagalera <dagalera.dev@gmail.com>
2024-09-07 01:04:00 -0300

    feat: adicionando paleta de cores

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -6,6 +6,20 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      'verdeMentaClaro': '#B2F2A3',
+      'verdeAqua': '#5DDCA7',
+      'turquesaVivo': '#00C3B3',
+      'azulCeuProfundo': '#00A7C1',
+      'azulEletrico': '#008AC8',
+      'azulRoyal': '#006AC3',
+      'amareloPastel': '#F9E5A0',
+      'verdeLimaoClaro': '#C6CF84',
+      'verdeOlivaClaro': '#90B970',
+      'verdeFloresta': '#55A26A',
+      'verdeEsmeralda': '#008A5F',
+      'verdeMusgo': '#00715E',
+    }
   },
   plugins: [],
 }
