9040694398e5af49ddbda8e2627aec4f49214ae0
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-09 08:33:31 -0300

    feat: adicionando router

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,9 +1,18 @@
+import Header from './components/header'
+import Footer from './components/footer'
 
+import { Outlet } from 'react-router-dom'
 export default function App() {
 
   return (
     <>
-     
+     <div>
+      <Header/>
+     </div>
+     <div>
+      <Outlet />
+     </div>
+     <Footer />
     </>
   )
 }
--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -2,10 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Home from './pages/home.jsx'
+import Personagem from './pages/personagem.jsx'
+import Quiz from './pages/quiz.jsx'
+import Videos from './pages/videos.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      
+      { index: true, element: <Home/>},
+      { path: 'personagem', element: <Personagem/>},
+      { path: 'quiz', element: <Solucao/>},
+      { path: 'videos', element: <Videos/>},
+      { path: '*',element: <PageNotFound/>},
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>  
   </StrictMode>,
 )
