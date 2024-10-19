acdb193ea2e3ea5952584ac829548a53a19ea432
EnzzoMont <rm552616@fiap.com.br>
2024-10-19 16:14:50 -0300

    header_mini_styles

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,10 +6,10 @@ export default function Header() {
       <div className="container px-2 md:mx-auto flex h-full items-center justify-between">
         <div>LOGO</div>
         <nav className="flex justify-around gap-2 md:gap-6">
-          <Link to="/" className="hover:bg-lime-100">Home</Link>
-          <Link to="/personagem" className="hover:bg-lime-100">Personagem</Link>
-          <Link to="/quiz/1" className="hover:bg-lime-100">Quiz</Link>
-          <Link to="/videos" className="hover:bg-lime-100">Videos</Link>
+          <Link to="/" className="md:bg-verde-100 md:hover:bg-lime-100 md:active:bg-lime-200 md:focus:ring-2 md:focus:ring-verde-200 md:border md:px-4 md:py-2 rounded-3xl text-sm font-medium">HOME</Link>
+          <Link to="/personagem" className="md:bg-verde-100 md:hover:bg-lime-100 md:active:bg-lime-200 md:focus:ring-2 md:focus:ring-verde-200 md:border md:px-4 md:py-2 rounded-3xl text-sm font-medium">PERSONAGEM</Link>
+          <Link to="/quiz/1" className="md:bg-verde-100 md:hover:bg-lime-100 md:active:bg-lime-200 md:focus:ring-2 md:focus:ring-verde-200 md:border md:px-4 md:py-2 rounded-3xl text-sm font-medium">QUIZ</Link>
+          <Link to="/videos" className="md:bg-verde-100 md:hover:bg-lime-100 md:active:bg-lime-200 md:focus:ring-2 md:focus:ring-verde-200 md:border md:px-4 md:py-2 rounded-3xl text-sm font-medium">VIDEOS</Link>
         </nav>
       </div>
     </header>
